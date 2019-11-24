b562090871eda8b93b9745882fc2912112867e67
Max Malm <benjick@dumfan.net>
2019-11-24 14:35:11 +0100

    Simplify quote

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div class="card">
       <div class="card-content">
         <p class="title">
-          Vi kräver en total insyn på varenda gård och slakteri i Sverige.
+          Vi kräver total insyn på varenda gård och slakteri i Sverige.
         </p>
         <p class="subtitle">
           #totalinsyn
