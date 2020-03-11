5e1b31709e344057fe1da8202e0299cd2c816854
Max Malm <benjick@dumfan.net>
2020-03-11 22:11:23 +0100

    Remove unused file

--- components/PhotoStripImage.vue
+++ components/PhotoStripImage.vue
@@ -1,17 +1,11 @@
 <template>
   <div class="column" data-testid="thumbnail">
     <nuxt-img :src="`${image}.jpg`" image-style="thumbnail" />
-    <thumbnail v-show="false" :image="`${image}.jpg`" />
   </div>
 </template>
 
 <script>
-import Thumbnail from '~/components/Thumbnail'
-
 export default {
-  components: {
-    Thumbnail
-  },
   props: {
     image: String
   }
