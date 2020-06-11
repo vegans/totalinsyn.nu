1aaa87b984b08d90e8c855d7b3989108c1aed88f
Max Malm <benjick@dumfan.net>
2020-06-11 23:39:39 +0200

    03

--- pages/03.vue
+++ pages/03.vue
@@ -1,109 +1,38 @@
 <template>
   <div>
     <ti-hero
-      :title="title"
-      :subtitle="description"
-      image="/03/header.jpg"
+      :title="page.title"
+      :subtitle="`${page.location} - ${page.date}`"
+      :image="page.header.image"
       :full="false"
     >
-      Aktivister från kampanjen #totalinsyn sände direkt från sju olika
-      grisfabriker natten till idag.
+      {{ page.header.preamble }}
     </ti-hero>
-    <press-release
-      sub="Söndagen den 26 april 2020"
-      title="Stor samordnad aktion i natt"
-      :prefix="false"
-    >
-      <!-- eslint-disable-next-line vue/no-v-html -->
-      <div v-html="content" />
+
+    <press-release :sub="page.press.subtitle" :title="page.press.title">
+      <nuxt-content :document="page" />
     </press-release>
-    <panel :press="press" :livestream="livestream" />
+    <panel :press="page.press.links" :livestream="page.livestreams" />
     <photo-strip
-      v-for="(images, date) in dates"
-      :key="date"
+      v-for="(images, key) in page.images"
+      :key="key"
       :images="images"
-      :title="date"
+      :title="key"
     />
-    <video-embed url="https://www.youtube.com/embed/e1HTVN4Aly8?showinfo=0" />
-    <action-map :coordinates="coordinates" />
+    <video-embed v-if="page.video" :url="page.video" />
+    <action-map v-if="page.coordinates" :coordinates="page.coordinates" />
   </div>
 </template>
 
 <script>
-import content from '~/content/03.md'
 import Action from '~/mixins/Action.js'
 
 export default {
   mixins: [Action],
-  data() {
+  async asyncData({ $content }) {
+    const page = await $content('03').fetch()
     return {
-      content,
-      action: '#03',
-      url: 'https://totalinsyn.nu/03',
-      title: 'Total Insyn #03',
-      description: 'Sverige - 26 april, 2020',
-      coordinates: [
-        [11.97456, 57.70887], // Göteborg
-        [13.43854, 55.795551], // Eslöv
-        [13.99179, 55.987572], // Träne
-        [15.621373, 58.410809], // Linköping
-        [16.544809, 59.609901] // Västerås
-      ],
-      dates: {
-        '2020-04-26': ['/03/image1', '/03/image2', '/03/image3', '/03/image4']
-      },
-      press: [
-        {
-          name: 'Anlib',
-          url:
-            'https://anlib.se/nyheter/inrikes/exponerade-sju-grisfabriker-inatt/'
-        },
-        {
-          name: 'Tidningen Syre',
-          url:
-            'https://tidningensyre.se/2020/30-april/intrang-pa-grisfarmer-filmades/'
-        },
-        {
-          name: 'Skånska dagbladet',
-          url:
-            'https://www.skd.se/2020/04/27/djurrattsaktivister-trangde-in-pa-gard/'
-        },
-        {
-          name: 'ATL',
-          url:
-            'https://www.atl.nu/lantbruk/aktivister-inne-pa-sju-svenska-gardar/'
-        }
-      ],
-      livestream: [
-        {
-          name: 'Karlskrona',
-          url: 'https://www.facebook.com/totalinsyn/videos/660423254520877'
-        },
-        {
-          name: 'Göteborg',
-          url: 'https://www.facebook.com/totalinsyn/videos/1374354306082402'
-        },
-        {
-          name: 'Linköping #1',
-          url: 'https://www.facebook.com/totalinsyn/videos/242167156837961'
-        },
-        {
-          name: 'Linköping #2',
-          url: 'https://www.facebook.com/totalinsyn/videos/1626421280844810'
-        },
-        {
-          name: 'Linköping #3',
-          url: 'https://www.facebook.com/totalinsyn/videos/234228050995904'
-        },
-        {
-          name: 'Eslöv',
-          url: 'https://www.facebook.com/totalinsyn/videos/606201163441176'
-        },
-        {
-          name: 'Västerås',
-          url: 'https://www.facebook.com/totalinsyn/videos/284299409238805'
-        }
-      ]
+      page
     }
   }
 }
