70667ec883ab22d90a2d406f53befbea9279286a
Max Malm <benjick@dumfan.net>
2020-06-12 09:58:21 +0200

    Better tests

--- cypress/integration/percy.spec.ts
+++ cypress/integration/percy.spec.ts
@@ -2,81 +2,47 @@
 
 const scrollToBottom = require('scroll-to-bottomjs')
 
-context('Home page', () => {
-  it('should render', () => {
-    cy.visit('/')
-    cy.get('[data-testid="actions-menu"]').click()
-    cy.window().then(cyWindow => scrollToBottom({ remoteWindow: cyWindow }));
-    cy.wait(1000)
-    cy.percySnapshot()
+const actions = ['01', '02', '03', '04']
+const pages = [
+  {
+    url: '/',
+    openMenu: true
+  },
+  {
+    url: '/motion'
+  },
+  {
+    url: '/material'
+  },
+  {
+    url: '/om',
+    openMenu: true
+  }
+]
+
+context('Pages', () => {
+  pages.forEach((page) => {
+    it(`page ${page.url}`, () => {
+      cy.visit(page.url)
+      if (page.openMenu) {
+        cy.get('[data-testid="actions-menu"]').click()
+      }
+      cy.window().then((cyWindow) => scrollToBottom({ remoteWindow: cyWindow }))
+      cy.wait(100)
+      cy.percySnapshot()
+    })
   })
 })
 
-context('Action #01 page', () => {
-  it('should render images', () => {
-    cy.visit('/01')
-    cy.get('[data-testid="thumbnail"]')
-      .its('length')
-      .should('be.gt', 0)
-    cy.window().then(cyWindow => scrollToBottom({ remoteWindow: cyWindow }));
-    cy.percySnapshot()
-  })
-})
-
-context('Action #02 page', () => {
-  it('should render images', () => {
-    cy.visit('/02')
-    cy.get('[data-testid="thumbnail"]')
-      .its('length')
-      .should('be.gt', 0)
-    cy.window().then(cyWindow => scrollToBottom({ remoteWindow: cyWindow }));
-    cy.percySnapshot()
-  })
-})
-
-context('Action #03 page', () => {
-  it('should render images', () => {
-    cy.visit('/03')
-    cy.get('[data-testid="thumbnail"]')
-      .its('length')
-      .should('be.gt', 0)
-    cy.wait(10000)
-    cy.window().then(cyWindow => scrollToBottom({ remoteWindow: cyWindow }));
-    cy.percySnapshot()
-  })
-})
-
-context('Action #04 page', () => {
-  it('should render images', () => {
-    cy.visit('/04')
-    cy.get('[data-testid="thumbnail"]')
-      .its('length')
-      .should('be.gt', 0)
-    cy.window().then(cyWindow => scrollToBottom({ remoteWindow: cyWindow }));
-    cy.percySnapshot()
-  })
-})
-
-context('Motionen', () => {
-  it('should render', () => {
-    cy.visit('/motion')
-    cy.window().then(cyWindow => scrollToBottom({ remoteWindow: cyWindow }));
-    cy.percySnapshot()
-  })
-})
-
-context('Material', () => {
-  it('should render', () => {
-    cy.visit('/material')
-    cy.window().then(cyWindow => scrollToBottom({ remoteWindow: cyWindow }));
-    cy.percySnapshot()
-  })
-})
-
-context('About', () => {
-  it('should render', () => {
-    cy.visit('/om')
-    cy.get('[data-testid="actions-menu"]').click()
-    cy.percySnapshot()
+context('Action pages', () => {
+  actions.forEach((page) => {
+    it(`action #${page}`, () => {
+      cy.visit(page)
+      cy.get('[data-testid="thumbnail"]')
+        .its('length')
+        .should('be.gt', 0)
+      cy.window().then((cyWindow) => scrollToBottom({ remoteWindow: cyWindow }))
+      cy.percySnapshot()
+    })
   })
 })
