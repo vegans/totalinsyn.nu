75929e0d7a776e87ad5bc12bdd94bfc2603feb31
Max Malm <benjick@dumfan.net>
2020-06-12 10:25:40 +0200

    Remove unused code

--- nuxt.config.js
+++ nuxt.config.js
@@ -135,22 +135,4 @@ export default {
         })
     }
   }
-  // content: {
-  //   async ready() {
-  //     const { $content } = require('@nuxt/content')
-  //     const files = await $content()
-  //       .only(['slug'])
-  //       .fetch()
-  //     console.log(files)
-  //   },
-  //   async routes() {
-  //     // TODO: Remove with 2.13
-  //     const { $content } = require('@nuxt/content')
-  //     const files = await $content()
-  //       .only(['path'])
-  //       .fetch()
-
-  //     return files.map((file) => (file.path === '/index' ? '/' : file.path))
-  //   }
-  // }
 }
