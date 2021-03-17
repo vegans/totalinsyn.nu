dd883a7ef536c3f318b9095de4ed4560a15b61a0
Max Malm <benjick@dumfan.net>
2021-03-17 17:50:07 +0100

    Change board members page

--- pages/om/styrelsen.vue
+++ pages/om/styrelsen.vue
@@ -1,45 +1,52 @@
 <template>
   <div>
-    <div class="pb-5 border-b border-gray-200">
-      <h3 class="text-lg leading-6 font-medium text-gray-900">
-        {{ page.title }}
-      </h3>
-      <p class="mt-2 max-w-4xl text-sm text-gray-500">
-        <nuxt-content :document="page" />
-      </p>
-    </div>
-    <ul
-      class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5"
-    >
-      <li
-        v-for="member in boardMembers"
-        :key="member.name"
-        class="col-span-1 flex flex-col text-center bg-white rounded-lg shadow divide-y divide-gray-200"
+    <div class="bg-white">
+      <div
+        class="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8 lg:py-24"
       >
-        <div class="flex-1 flex flex-col p-8">
-          <img
-            class="w-32 h-32 flex-shrink-0 mx-auto bg-black rounded-full"
-            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=60"
-            alt=""
-          />
-          <h3 class="mt-6 text-gray-900 text-sm font-medium">
-            {{ member.name }}
-          </h3>
-          <dl class="mt-1 flex-grow flex flex-col justify-between">
-            <dt class="sr-only">Roll</dt>
-            <dd class="text-gray-500 text-sm">{{ member.role }}</dd>
-          </dl>
+        <div class="space-y-12">
+          <div
+            class="space-y-5 sm:mx-auto sm:max-w-xl sm:space-y-4 lg:max-w-5xl"
+          >
+            <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">
+              {{ page.title }}
+            </h2>
+            <p class="text-xl text-gray-500">
+              <nuxt-content :document="page" />
+            </p>
+          </div>
+          <ul
+            class="mx-auto space-y-16 sm:grid sm:grid-cols-2 sm:gap-16 sm:space-y-0 lg:grid-cols-3 lg:max-w-5xl"
+          >
+            <li v-for="member in boardMembers" :key="member.slug">
+              <div class="space-y-6">
+                <img
+                  class="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56"
+                  :src="
+                    require(`~/assets/styrelsen/${member.slug}.jpg?resize&size=600`)
+                  "
+                  alt=""
+                />
+                <div class="space-y-2">
+                  <div class="text-lg leading-6 font-medium space-y-1">
+                    <h3>{{ member.name }}</h3>
+                    <p class="text-yellow-600">{{ member.role }}</p>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
         </div>
-      </li>
-    </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  async asyncData({ $content, route }) {
+  async asyncData({ $content }) {
     const boardMembers = await $content('boardMembers')
-      .only(['name', 'role'])
+      .only(['slug', 'name', 'role'])
       .sortBy('order', 'asc')
       .fetch()
     const page = await $content('om/styrelsen').fetch()
