d41427f522eb94aacc244a2fe4056f7c4fde9426
Max Malm <benjick@dumfan.net>
2021-03-17 23:19:15 +0100

    Add engage widget

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,14 @@
-<template><div></div></template>
+<template>
+  <div>
+    <Engage
+      title="Engagera dig!"
+      cta="Hitta din lokalgrupp"
+      to="/om/lokalgrupper"
+      body="Varius facilisi mauris sed sit. Non sed et duis dui leo, vulputate id malesuada non. Cras aliquet purus dui laoreet diam sed lacus, fames."
+      asset="engage.jpg"
+    />
+  </div>
+</template>
 
 <script>
 export default {
