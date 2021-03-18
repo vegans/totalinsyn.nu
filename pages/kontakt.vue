bf69610ed6cfe2463f119e7c27d9e8198ab684b7
Max Malm <benjick@dumfan.net>
2021-03-18 03:30:02 +0100

    Updat contact form

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -1,5 +1,64 @@
 <template>
   <div>
+    <div v-if="success" class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+      <div class="p-2 rounded-lg bg-yellow-500 shadow-lg sm:p-3">
+        <div class="flex items-center justify-between flex-wrap">
+          <div class="w-0 flex-1 flex items-center">
+            <span class="flex p-2 rounded-lg bg-yellow-600">
+              <!-- Heroicon name: outline/speakerphone -->
+              <svg
+                class="h-6 w-6 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"
+                />
+              </svg>
+            </span>
+            <p class="ml-3 font-medium text-white truncate">
+              <span class="md:hidden">
+                Tack för ditt meddelande!
+              </span>
+              <span class="hidden md:inline">
+                Tack för ditt meddelande! Vi återkommer snarast.
+              </span>
+            </p>
+          </div>
+          <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-2">
+            <button
+              type="button"
+              class="-mr-1 flex p-2 rounded-md hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-white"
+              @click="close()"
+            >
+              <span class="sr-only">Dismiss</span>
+              <!-- Heroicon name: outline/x -->
+              <svg
+                class="h-6 w-6 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
     <div
       class="max-w-7xl mx-auto py-4 px-4 sm:py-6 sm:px-6 lg:px-8 rounded-3xl"
     >
@@ -194,7 +253,9 @@
               Skicka ett meddelande
             </h3>
             <form
-              action="#"
+              data-netlify="true"
+              name="contact"
+              action="/kontakt#tack"
               method="POST"
               class="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
             >
@@ -326,6 +387,22 @@ export default {
     const page = await $content('pages/kontakt').fetch()
     return {
       page
+    }
+  },
+  data() {
+    return {
+      success: false
+    }
+  },
+  mounted() {
+    if (window.location.hash === '#tack') {
+      this.success = true
+    }
+  },
+  methods: {
+    close() {
+      this.success = false
+      history.replaceState(null, null, ' ')
     }
   }
 }
